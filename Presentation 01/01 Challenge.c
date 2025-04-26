1a744871e45b5543674b46c995de74c951034f13
sarin00 <138829008+sarin00@users.noreply.github.com>
2025-04-26 16:29:13 +0200

    v0.1

--- Presentation 01/01 Challenge.c
+++ Presentation 01/01 Challenge.c
@@ -1,5 +1,2 @@
 String query = String.format("SELECT * FROM users WHERE usr='%s' AND pwd='%s'", usr, pwd);
-Connection conn = db.getConn();
-Statement stmt = conn.createStatement();
 
-ResultSet rs = stmt.executeQuery(query);
